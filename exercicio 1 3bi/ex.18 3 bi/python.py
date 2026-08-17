c698542286194753fcc3962ddc6bd2900c835da0
Toki no Github <thiagomanuelolivei@gmail.com>
2026-08-17 11:13:42 -0300

    Add files via upload

---
+++ exercicio 1 3bi/ex.18 3 bi/python.py
@@ -0,0 +1,9 @@
+numero = int(input("Coloque um número: "))
+if numero < 10 and numero == 10:
+    print("Não é entre 10 e 20.")
+elif numero == 20:
+    print("Não é entre 10 e 20.")
+elif numero < 20:
+    print("Esta entre 10 e 20")
+else:
+    print("Não é entre 10 e 20, é muito maior.")
